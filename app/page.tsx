df7bbb441914ba2db1db509fd0e32d63a9ae299c
badiniibrahim <sawadogo.badiniibrahim@yahoo.fr>
2023-09-28 14:18:14 +0200

    add img

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Blog prompt
+        Blog
         <br className="max-md:hidden" />
       </h1>
       <p className="desc text-center">
